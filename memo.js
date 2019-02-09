f79f5b2b464fddef77ee7f041b81b6317ab1bf61
Robert Dopieralski <abc507446692@gmail.com>
2019-02-09 21:50:51 +0100

    sudden change of mind with game-logic

--- memo.js
+++ memo.js
@@ -1,22 +1,50 @@
 let gameBoard = document.querySelector('.board')
+let cards = 16
+let values = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
+let cardSet
 
-let cardSet = Array.from({ length: 4 }, item => Array.from({ length: 4 }, place => []))
+render()
 
-renderCards()
-
-function renderCards() {
-    cardSet
-        .forEach(line => {
-            let i = 0
-            line
-                .forEach(position => {
-                    let cardNode = document.createElement('div')
-                    cardNode.classList.add('cardUpsideDown')
-                    gameBoard.appendChild(cardNode)
-                    cardNode.textContent = i + 1
-                    i++
-                })
-        })
+function render(){
+    generateCardSet()
+    console.log(cardSet)
 }
 
-// console.log(cardSet)
+function generateCardSet(){
+    cardSet = Array.from({length:cards}, el => ['#', pickValue()[0]])
+}
+
+function pickValue(){
+    return values.splice(0,1)
+}
+
+
+// in case i would have another change of mind i will leave this stuff here for later
+
+// let cardSet = Array.from({ length: 4 }, item => Array.from({ length: 4 }, place => []))
+
+// renderCards()
+
+// function renderCards() {
+//     cardSet
+//         .forEach(line => {
+//             let i = 0
+//             line
+//                 .forEach(position => {
+//                     let cardNode = document.createElement('div')
+//                     cardNode.classList.add('cardUpsideDown')
+//                     cardNode.addEventListener('click', turnAround)
+//                     gameBoard.appendChild(cardNode)
+//                     cardNode.textContent = i + 1
+//                     i++
+//                 })
+//         })
+// }
+
+// function turnAround(event) {
+//     let card = event.target
+//     //card.textContent = 
+
+//     console.log(card)
+// }
+// // console.log(cardSet)
