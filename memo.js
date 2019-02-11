e611b926bbf09455a46b29310ea890fc52ac6d4d
Robert Dopieralski <abc507446692@gmail.com>
2019-02-11 21:14:26 +0100

    WIP: You can play entire lifespan of Yours

--- memo.js
+++ memo.js
@@ -1,46 +1,101 @@
 let gameBoard = document.querySelector('.board')
 let cards = 16
-let values = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8]
+const values = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
 let cardSet
+let score = 0
+let choice
 
 render()
 
-function render(){
+function render() {
+    choice = templateArray()
     generateCardSet()
     displayCards()
     console.log(cardSet)
 }
 
-function turnAround(event){
-    let card = event.target
-    card.classList.toggle('cardUpsideDown')
-    card.classList.toggle('cardRightWay')
+function templateArray(){
+    let array = Array.from(values)
+    return array
 }
 
-function displayCards(){
+function vanishPair() {
+    let active = document.querySelectorAll('.active')
+    active
+        .forEach(card => {
+            card.parentElement.removeChild(card)
+        })
+    checkIfEndOfTheGame()
+}
+
+function flipCards() {
+    let active = document.querySelectorAll('.active')
+    active
+        .forEach(card => {
+            card.classList.toggle('cardUpsideDown')
+            card.classList.toggle('cardRightWay')
+            card.classList.toggle('active')
+        })
+}
+
+function checkIfEndOfTheGame() {
+    let cards = document.querySelectorAll('.card')
+    if (cards.length === 0) {
+        render()
+    }
+}
+
+function checkIfPair() {
+    let active = document.querySelectorAll('.active')
+    if (active.length === 2) {
+        let cardOne = active[0].innerHTML
+        let cardTwo = active[1].innerHTML
+        if (cardOne === cardTwo) {
+            score++
+            setTimeout(vanishPair, 1000)
+        }
+        else {
+            setTimeout(flipCards, 3000)
+        }
+    }
+}
+
+function turnAround(event) {
+    let active = document.querySelectorAll('.active')
+    if (active.length < 2) {
+        let card = event.target
+        card.classList.toggle('cardUpsideDown')
+        card.classList.toggle('cardRightWay')
+        card.classList.toggle('active')
+        checkIfPair()
+    }
+}
+
+function displayCards() {
     cardSet
-    .forEach(card =>{
-        let cardNode = document.createElement('div')
-        cardNode.classList.add('cardUpsideDown')
-        cardNode.addEventListener('click', turnAround)
-        let cardInnerText = document.createElement('p')
-        cardInnerText.textContent = card
-        cardNode.appendChild(cardInnerText)
-        gameBoard.appendChild(cardNode)
-    })
+        .forEach(card => {
+            let cardNode = document.createElement('div')
+            cardNode.classList.add('card')
+            cardNode.classList.add('cardUpsideDown')
+            cardNode.addEventListener('click', turnAround)
+            let cardInnerText = document.createElement('p')
+            cardInnerText.textContent = card
+            cardNode.appendChild(cardInnerText)
+            gameBoard.appendChild(cardNode)
+        })
 }
 
-function generateCardSet(){
-    cardSet = Array.from({length:cards}, el => pickValue()[0])
+function generateCardSet() {
+    cardSet = Array.from({ length: cards }, el => pickValue()[0])
 }
 
-function pickValue(){
-    pick = Math.floor(Math.random() * values.length)
-    let value = values.splice(pick,(1))
-    if (value !== undefined){
+function pickValue() {
+    pick = Math.floor(Math.random() * choice.length)
+    let value = choice.splice(pick, (1))
+    if (value !== undefined) {
         return value
     }
-    else{
+    else {
         pickValue()
     }
 }
